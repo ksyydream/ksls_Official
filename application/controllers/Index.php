1000e43556dfdcfebd664d7fa0555c5cea2d505c
ksyydream <249499109@qq.com>
2017-07-20 16:00:22 +0800

    11

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Join extends MY_Controller {
+class Index extends MY_Controller {
 
 	/**
 	 * Index Page for this controller.
